66eb8f15f7e357db6c38f26ee610ee0618b5696c
Marina123456 <226598@niuitmo.ru>
2017-05-22 13:21:59 +0300

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,37 @@
+<?php 
+header('Content-Type: text/html; charset=utf-8');
+if ($_GET["name"]){
+	$message=messageGet($_GET["name"],$_GET["message"]);
+	if (sendMail('marjnazhjrnova@yandex.ru',$message,$_GET["from"])) {
+		echo json_decode(array('status'=>'1'));
+	} else echo json_decode(array('status'=>'0'));
+}
+function messageGet($name,$message){
+	return 'Имя: '.$name. "\r\n"."Сообщение: ".$message;
+}
+function sendMail($to,$message,$from){
+
+     
+    $subject = getParam('with_portfolio');
+
+
+    $body = '<html>'. "\n";
+    $body .= '<head> <title></title>'. "\n";
+    $body .= '</head>'. "\n";
+    $body .= '<body>'. "\n";
+    $body .= $message. "\n";    
+    //$body .= chunk_split(base64_encode($message))."\n";
+    //$body .= "--".$boundary ."--\n";
+    $body .= '</body>'. "\n";
+    $body .= '</html>'. "\n";
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+
+    // Дополнительные заголовки
+    $headers .= 'To: Client <'.$to.'>' . "\r\n";
+    $headers .= 'From: Portfolio site <'.$from.'>' . "\r\n";
+
+    return mail($to, $subject, $body, $headers,"-f".$from);
+}
+
+?>
